8a0379c78ce9a7128144b7d4f50729c8f184d91d
Esko Luontola <esko@luontola.fi>
2022-11-10 20:58:45 +0200

    Singletons: DAO

--- test/testable.test.mjs
+++ test/testable.test.mjs
@@ -4,8 +4,38 @@ import {
   diceHandValue,
   diceRoll,
   parsePeopleCsv,
+  PasswordService,
   readUtf8File,
+  UserDao,
 } from "../src/testable.mjs";
+
+describe("Singletons: DAO", () => {
+  const userId = 123;
+  let users;
+  let service;
+  beforeEach(() => {
+    users = new UserDao();
+    service = new PasswordService(users);
+  });
+
+  it("change password", () => {
+    users.save({ userId, password: "old-pw" });
+
+    service.changePassword(userId, "old-pw", "new-pw");
+
+    expect(users.getById(userId).password).to.equal("new-pw");
+  });
+
+  it("old password did not match", () => {
+    users.save({ userId, password: "old-pw" });
+
+    expect(() => {
+      service.changePassword(userId, "wrong-pw", "new-pw");
+    }).to.throw(Error, "wrong old password");
+
+    expect(users.getById(userId).password).to.equal("old-pw");
+  });
+});
 
 describe("File system: parse CSV file", () => {
   it("read file", async () => {
