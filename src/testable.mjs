7c3c17bbd5da903bfd2990a07e409fc578a68a79
Esko Luontola <esko@luontola.fi>
2022-11-10 20:58:45 +0200

    Singletons: DAO

--- src/testable.mjs
+++ src/testable.mjs
@@ -1,7 +1,45 @@
 import { readFile } from "fs/promises";
 import { parse } from "csv-parse/sync";
 
-// TODO: Singletons or global variables
+// Singletons: DAO
+
+function clone(obj) {
+  // TODO: could also use structuredClone, but it would require upgrading to Node.js 17.0
+  if (obj) {
+    return { ...obj };
+  }
+  return null;
+}
+
+export class UserDao {
+  users = {};
+
+  getById(userId) {
+    return clone(this.users[userId]);
+  }
+
+  save(user) {
+    this.users[user.userId] = clone(user);
+  }
+}
+
+export class PasswordService {
+  constructor(users) {
+    this.users = users;
+  }
+
+  changePassword(userId, oldPassword, newPassword) {
+    const user = this.users.getById(userId);
+    // This is code isn't meant to be secure. Always store passwords using bcrypt or similar algorithm
+    // which is designed for passwords. See https://auth0.com/blog/hashing-passwords-one-way-road-to-security/
+    if (oldPassword !== user.password) {
+      throw new Error("wrong old password");
+    } else {
+      user.password = newPassword;
+    }
+    this.users.save(user);
+  }
+}
 
 // File system: parse CSV
 
