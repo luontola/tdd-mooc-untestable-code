f40bebeabc7ee6bfccee95dff6adfe3849b974dd
Esko Luontola <esko@luontola.fi>
2022-11-10 20:05:00 +0200

    File system: parse CSV file

--- src/testable.mjs
+++ src/testable.mjs
@@ -1,5 +1,32 @@
+import { readFileSync } from "fs";
+import { parse } from "csv-parse/sync";
+
 // TODO: Singletons or global variables
-// TODO: File system
+
+// File system: parse CSV
+
+export function readFile(path) {
+  return readFileSync(path).toString();
+}
+
+export function parsePeopleCsv(csvData) {
+  const records = parse(csvData, {
+    skip_empty_lines: true,
+    trim: true,
+  });
+  return records.map(([firstName, lastName, age, gender]) => {
+    const person = {
+      firstName,
+      lastName,
+      gender: gender.charAt(0).toLowerCase(),
+    };
+    if (age !== "") {
+      person.age = parseInt(age);
+    }
+    return person;
+  });
+}
+
 // TODO: Network sockets
 
 // Time: days until Christmas
