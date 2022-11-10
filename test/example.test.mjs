4ce9b63b25fab56146c29dcb46834edde0fbcb39
Esko Luontola <esko@luontola.fi>
2022-11-10 18:54:40 +0200

    Cleanup

--- test/example.test.mjs
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { sum } from "../src/example.mjs";
-
-describe("Example test fixture", () => {
-  it("Example test", () => {
-    expect(sum(1, 2)).to.equal(3);
-  });
-});
