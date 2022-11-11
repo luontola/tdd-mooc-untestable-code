036615f14cb6403c02241253b31820a3eb4f446d
Esko Luontola <esko@luontola.fi>
2022-11-11 20:30:43 +0200

    Untestable 1

---
+++ test/untestable1.test.mjs
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { daysUntilChristmas } from "../src/untestable1.mjs";
+
+describe("Untestable 1: days until Christmas", () => {
+  it("todo", () => {
+    // TODO: write proper tests
+    expect(daysUntilChristmas()).to.be.a("number");
+  });
+});
