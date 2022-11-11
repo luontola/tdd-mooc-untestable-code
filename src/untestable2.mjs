6e3cfa8538ef6771adc86d9da485d4a5b2e4bac2
Esko Luontola <esko@luontola.fi>
2022-11-11 20:30:48 +0200

    Untestable 2

---
+++ src/untestable2.mjs
@@ -0,0 +1,17 @@
+function diceRoll() {
+  const min = 1;
+  const max = 6;
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+export function diceHandValue() {
+  const die1 = diceRoll();
+  const die2 = diceRoll();
+  if (die1 === die2) {
+    // one pair
+    return 100 + die1;
+  } else {
+    // high die
+    return Math.max(die1, die2);
+  }
+}
---
+++ test/untestable2.test.mjs
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { diceHandValue } from "../src/untestable2.mjs";
+
+describe("Untestable 2: a dice game", () => {
+  it("todo", () => {
+    // TODO: write proper tests
+    expect(diceHandValue()).to.be.a("number");
+  });
+});
