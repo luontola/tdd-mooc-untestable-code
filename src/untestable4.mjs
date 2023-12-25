04e20eb5e59e81fd3d23e4ec81ba358385a3ff3b
Esko Luontola <esko@luontola.fi>
2023-12-25 13:16:12 +0200

    Code reformat

--- src/untestable4.mjs
+++ src/untestable4.mjs
@@ -32,7 +32,7 @@ export class PostgresUserDao {
       `select user_id, password_hash
        from users
        where user_id = $1`,
-      [userId]
+      [userId],
     );
     return rows.map(this.#rowToUser)[0] || null;
   }
@@ -43,7 +43,7 @@ export class PostgresUserDao {
        values ($1, $2)
        on conflict (user_id) do update
            set password_hash = excluded.password_hash`,
-      [user.userId, user.passwordHash]
+      [user.userId, user.passwordHash],
     );
   }
 }
